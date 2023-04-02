c4925e2c0daf5e5a0a083b889f0bccbc40384caa
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-04-02 04:44:23 +0200

    linters fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'messages/index'
-    end
-    get 'v1/message'
-  end
-  get 'root/index'
+  root 'root#index'
+  get 'api/v1/messages', to: 'api/v1/messages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "root#index"
+  # root "articles#index"
 end
